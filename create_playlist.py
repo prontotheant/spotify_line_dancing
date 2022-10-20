7aac043eb216d8443e665955c55095fd47dd16e0
apatterson <alexpatterson2019@gmail.com>
2022-10-20 12:37:14 -0400

    -m "created some of needed files for project"

---
+++ create_playlist.py
@@ -0,0 +1,8 @@
+"""
+file: create_playlist.py
+author: A. Patterson, J. Parra
+purpose: create a playlist, populate it with songs taken from a source
+"""
+
+if __name__ == '__main__':
+    api_token = input("Please enter your token:")
---
+++ grab_new_song.py
@@ -0,0 +1,8 @@
+"""
+file: grab_new_song.py
+author: A. Patterson, J. Parra
+purpose: search for new song through spotify api, prepare to add to database
+"""
+
+if __name__ == '__main__':
+    api_token = input("Please enter your token:")
