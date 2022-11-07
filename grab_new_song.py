cb6ae79967534938b3fe0398699cb2449194d543
Alex Patterson <69934013+prontotheant@users.noreply.github.com>
2022-11-07 11:04:47 -0500

    Delete grab_new_song.py

--- grab_new_song.py
+++
@@ -1,8 +0,0 @@
-"""
-file: grab_new_song.py
-author: A. Patterson, J. Parra
-purpose: search for new song through spotify api, prepare to add to database
-"""
-
-if __name__ == '__main__':
-    api_token = input("Please enter your token:")
