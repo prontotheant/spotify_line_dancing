bd3a6715c6b09a61e9ae66f805669cb478464fa8
Alex Patterson <69934013+prontotheant@users.noreply.github.com>
2022-11-07 11:04:16 -0500

    Delete create_playlist.py

--- create_playlist.py
+++
@@ -1,8 +0,0 @@
-"""
-file: create_playlist.py
-author: A. Patterson, J. Parra
-purpose: create a playlist, populate it with songs taken from a source
-"""
-
-if __name__ == '__main__':
-    api_token = input("Please enter your token:")
